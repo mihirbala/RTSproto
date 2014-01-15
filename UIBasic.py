f38a59a0899f3c8af1b953503d0bf4beac4b00cf
mihirbala <mbala_42@yahoo.com>
2014-01-14 19:55:01 -0500

    Player Movement and Display

--- UIBasic.py
+++ UIBasic.py
@@ -12,7 +12,7 @@ import random
 import math
 import time
 
-class unit:
+class monst:
     def __init__(self, starting_pos, unit_type):
         self.current = starting_pos
         self.unit_type = unit_type
@@ -23,15 +23,27 @@ class unit:
     def change_pos(self, next_pos):
         self.current = next_pos
 
+class player:
+    def __init__(self, starting_pos):
+        self.current_pos = starting_pos
+    
+    def get_current(self):
+        return self.current_pos
+    
+    def change_pos(self, next_pos):
+        self.current_pos = next_pos        
+
 grid_to_node = {}
 grid = []
-square = unit((0,0), "Square")
+monster = monst((0,0), "Monster")
+player = player((5,5))
 
 black    = (   0,   0,   0)
 white    = ( 255, 255, 255)
 green    = (   0, 255,   0)
 red      = ( 255,   0,   0)
 blue     = (   0,   0, 255)
+orange   = ( 244, 164, 96)
 
 ROWS = 25
 COLUMNS = 25
@@ -48,7 +60,6 @@ graph = Graph.Graph()
 # This sets the margin between each cell
 margin = 5
 
-print "init grid"
 for row in range(ROWS):
     grid.append([])
     for column in range(COLUMNS):
@@ -61,12 +72,14 @@ def reset_grid():
             if grid[row][column] != 3:
                 grid[row][column] = 0
     grid[0][0] = 1
-    
+
+player_position = player.get_current()
+grid[player_position[0]][player_position[1]] = 4    
 grid[0][0] = 1
 
 def draw():
-    print "drawing"
     screen.fill(black)
+    grid[player.current_pos[0]][player.current_pos[1]] = 4
     for row in range(ROWS):
         for column in range(COLUMNS):
             color = white
@@ -76,6 +89,8 @@ def draw():
                 color = green
             elif grid[row][column] == 3:
                 color = blue
+            elif grid[row][column] == 4:
+                color = orange
             pygame.draw.rect(screen,
                              color,
                              [(margin+width)*column+margin,
@@ -87,14 +102,14 @@ def draw():
     # Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
     
-print "init high cost"
-for i in range(int(math.ceil(0.5 * (ROWS * COLUMNS)))):
+
+for i in range(int(math.ceil(0.25 * (ROWS * COLUMNS)))):
     highx = random.randint(0, ROWS-1)
     highy = random.randint(0, COLUMNS-1)
     grid[highx][highy] = 3
 
 node_grid = []
-print "init node grid"
+
 for i in range(ROWS):
     node_grid.append([])
     for j in range(COLUMNS):
@@ -105,7 +120,6 @@ for i in range(ROWS):
         node_grid[i].append(node)
         graph.add_node(node)
 
-print "conver to graph(edges)"
 for i in range(ROWS):
     for j in range(COLUMNS):
         if j > 0: # has a left neighbor
@@ -125,7 +139,6 @@ for i in range(ROWS):
         if j < COLUMNS-1 and i < ROWS-1: # has bottom right neighbor
             graph.add_edge(node_grid[i][j], node_grid[i+1][j+1])
      
-print "init pygame" 
 pygame.init()
 size = [(width*COLUMNS)+(margin*(COLUMNS+1)), (height*ROWS)+(margin*(ROWS+1))]
 screen = pygame.display.set_mode(size)
@@ -139,12 +152,38 @@ done = False
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
-print "main event loop"
+#Handle hold down key event
+
 while not done:
     draw()
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
             done = True # Flag that we are done so we exit this loop
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                if player.current_pos[1] < COLUMNS-1:
+                    if grid[player.current_pos[0]][player.current_pos[1] + 1] != 3:
+                        player.change_pos((player.current_pos[0], player.current_pos[1] + 1))
+                        reset_grid()
+                        draw()
+            elif event.key == pygame.K_w:
+                if player.current_pos[0] > 0:
+                    if grid[player.current_pos[0] - 1][player.current_pos[1]] != 3:
+                        player.change_pos((player.current_pos[0] - 1, player.current_pos[1]))
+                        reset_grid()
+                        draw()
+            elif event.key == pygame.K_s:
+                if player.current_pos[0] < ROWS-1:
+                    if grid[player.current_pos[0] + 1][player.current_pos[1]] != 3:
+                        player.change_pos((player.current_pos[0] + 1, player.current_pos[1]))
+                        reset_grid()
+                        draw()
+            elif event.key == pygame.K_a:
+                if player.current_pos[1] > 0:
+                    if grid[player.current_pos[0]][player.current_pos[1] - 1] != 3:
+                        player.change_pos((player.current_pos[0], player.current_pos[1] - 1))
+                        reset_grid()
+                        draw()
         elif event.type == pygame.MOUSEBUTTONDOWN:
             reset_grid()
             # User clicks the mouse. Get the position
@@ -156,17 +195,16 @@ while not done:
             grid[row][column] = 2
             #print("Click", pos, "Grid coordinates: ", row, column)
             path = Astar.Astar(node_grid[0][0], node_grid[row][column], graph)
-            '''for node in path:
+            for node in path:
                 #print node.x, node.y
                 if grid[node.x][node.y] != 3:
-                    grid[node.x][node.y] = 2'''
+                    grid[node.x][node.y] = 2
             grid[0][0] = 1 # color the start red
             for node in path:
-                square.change_pos((node.x, node.y))                                    
+                monster.change_pos((node.x, node.y))                                    
                 grid[node.x][node.y] = 1
                 #time.sleep(2)
                 draw()
-                print "done drawing"
                 
 
     
